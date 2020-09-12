2aec9b0898c96e8310751f8fcfbcaea5046ca5f7
Kevin Wang <kwangsan@gmail.com>
2020-09-12 09:25:37 -0400

    wasm piano POC

--- js/index.js
+++ js/index.js
@@ -1,5 +1,156 @@
-import("../pkg/index.js")
-  .then((mod) => {
-    mod.run(4, [2]);
+const piano = {
+  A0: 27.5,
+  "A♯0/B♭0": 29.13524,
+  B0: 30.86771,
+  C1: 32.7032,
+  "C♯1/D♭": 34.64783,
+  D1: 36.7081,
+  "D♯1/E♭1": 38.89087,
+  E1: 41.20344,
+  F1: 43.65353,
+  "F♯1/G♭": 46.2493,
+  G1: 48.99943,
+  "G♯1/A♭": 51.91309,
+  A1: 55.0,
+  "A♯1/B♭1": 58.27047,
+  B1: 61.73541,
+  C2: 65.40639,
+  "C♯2/D♭2": 69.29566,
+  D2: 73.41619,
+  "D♯2/E♭2": 77.78175,
+  E2: 82.40689,
+  F2: 87.30706,
+  "F♯2/G♭2": 92.49861,
+  G2: 97.99886,
+  "G♯2/A♭2": 103.8262,
+  A2: 110.0,
+  "A♯2/B♭2": 116.5409,
+  B2: 123.4708,
+  C3: 130.8128,
+  "C♯3/D♭3": 138.5913,
+  D3: 146.8324,
+  "D♯3/E♭3": 155.5635,
+  E3: 164.8138,
+  F3: 174.6141,
+  "F♯3/G♭3": 184.9972,
+  G3: 195.9977,
+  "G♯3/A♭3": 207.6523,
+  A3: 220.0,
+  "A♯3/B♭3": 233.0819,
+  B3: 246.9417,
+  C4: 261.6256,
+  "C♯4/D♭4": 277.1826,
+  D4: 293.6648,
+  "D♯4/E♭4": 311.127,
+  E4: 329.6276,
+  F4: 349.2282,
+  "F♯4/G♭4": 369.9944,
+  G4: 391.9954,
+  "G♯4/A♭4": 415.3047,
+  A4: 440.0,
+  "A♯4/B♭4": 466.1638,
+  B4: 493.8833,
+  C5: 523.2511,
+  "C♯5/D♭5": 554.3653,
+  D5: 587.3295,
+  "D♯5/E♭5": 622.254,
+  E5: 659.2551,
+  F5: 698.4565,
+  "F♯5/G♭5": 739.9888,
+  G5: 783.9909,
+  "G♯5/A♭5": 830.6094,
+  A5: 880.0,
+  "A♯5/B♭5": 932.3275,
+  B5: 987.7666,
+  C6: 1046.502,
+  "C♯6/D♭6": 1108.731,
+  D6: 1174.659,
+  "D♯6/E♭6": 1244.508,
+  E6: 1318.51,
+  F6: 1396.913,
+  "F♯6/G♭6": 1479.978,
+  G6: 1567.982,
+  "G♯6/A♭6": 1661.219,
+  A6: 1760.0,
+  "A♯6/B♭6": 1864.655,
+  B6: 1975.533,
+  C7: 2093.005,
+};
+
+import("../pkg")
+  .then((rust_module) => {
+    let fm = null;
+
+    const play_button = document.getElementById("play");
+    play_button.addEventListener("click", (event) => {
+      if (fm === null) {
+        play_button.innerHTML = "⏸";
+        fm = new rust_module.FmOsc();
+        fm.set_note(50);
+        fm.set_fm_frequency(2);
+        fm.set_fm_amount(0);
+        fm.set_gain(0.8);
+      } else {
+        play_button.innerHTML = "▶️";
+        fm.free();
+        fm = null;
+      }
+    });
+    const pianoDiv = document.getElementById("piano");
+    pianoDiv.style.display = "flex";
+    pianoDiv.style.flexDirection = "row";
+    pianoDiv.style.position = "relative";
+
+    Object.entries(piano)
+      .sort(([, pA], [, pB]) => {
+        return pA < pB;
+      })
+      .map(([key, value], i) => {
+        let isBlackKey = key.includes("/");
+        let div = document.createElement("kbd");
+
+        // div.style.border = "-1px inset black";
+        div.style.boxShadow = "inset 0px 0px 1px black";
+
+        if (!isBlackKey) {
+          div.style.width = "10px";
+          div.style.height = "80px";
+          // div.style.transform = "translateY(10px)";
+          const [note, num] = key;
+          div.innerText = note;
+        } else {
+          div.style.width = "10px";
+          div.style.height = "80px";
+          const [sharp, flat] = key.split("/");
+          const [note, sign, num] = sharp;
+          // div.innerText = note + sign;
+          div.style.background = "lightgrey";
+        }
+        let ignore = false;
+        div.addEventListener("mousedown", () => {
+          ignore = !ignore;
+          if (div.style.background !== "red") {
+            div.style.background = "red";
+          } else {
+            div.style.background = "inherit";
+          }
+        });
+        div.addEventListener("mouseover", () => {
+          if (fm && !ignore) {
+            fm.set_primary_frequency(parseInt(value));
+            // fm.set_note(parseInt(i + 12));
+            // fm.set_fm_frequency(parseFloat(value));
+          }
+        });
+        pianoDiv.appendChild(div);
+      });
+
+    // document.body.addEventListener("mousemove", (event) => {
+    //   console.log(event.clientX, event.clientY);
+    //   if (fm) {
+    //     fm.set_note(parseInt(event.clientX / 2));
+    //     fm.set_fm_frequency(parseFloat(event.clientX));
+    //   }
+    // })
   })
   .catch(console.error);
--- src/lib.rs
+++ src/lib.rs
@@ -1,132 +1,152 @@
-use js_sys::{Array, Date, Number};
 use wasm_bindgen::prelude::*;
-use wasm_bindgen::JsCast;
-use web_sys::{console, Document, Element, HtmlElement, Window};
+use web_sys::{AudioContext, OscillatorType};
 
-// #[wasm_bindgen(start)]
-#[wasm_bindgen]
-pub fn run(num: Number, arr: Array) -> Result<(), JsValue> {
-    console::log_1(&format!("run::{:?}", &num).into());
-    console::log_1(&format!("run::{:?}", &arr).into());
-    console::group_1(&JsValue::from_str("Hello!"));
-    arr.for_each(&mut |obj: JsValue, idx: u32, arr: js_sys::Array| {
-        // Two std::fmt compiler errors:
-        // `wasm_bindgen::JsValue` doesn't implement `std::fmt::Display`
-        // `js_sys::Array` doesn't implement `std::fmt::Display`
-        console::log_1(
-            &format!("{arr:?}[{idx}] = {obj:?}", obj = obj, idx = idx, arr = arr).into(),
-        );
-    });
-    console::group_end();
-
-    let window: Window = web_sys::window().expect("should have a window in this context");
-    let document: Document = window.document().expect("window should have a document");
-
-    // One of the first interesting things we can do with closures is simply
-    // access stack data in Rust!
-    let array = Array::new();
-    array.push(&"Hello".into());
-    array.push(&1.into());
-    let mut first_item = None;
-    array.for_each(&mut |obj, idx, _arr| match idx {
-        0 => {
-            assert_eq!(obj, "Hello");
-            first_item = obj.as_string();
-        }
-        1 => assert_eq!(obj, 1),
-        _ => panic!("unknown index: {}", idx),
-    });
-    assert_eq!(first_item, Some("Hello".to_string()));
-
-    // Below are some more advanced usages of the `Closure` type for closures
-    // that need to live beyond our function call.
-
-    setup_clock(&window, &document)?;
-    setup_clicker(&document);
-
-    // And now that our demo is ready to go let's switch things up so
-    // everything is displayed and our loading prompt is hidden.
-    document
-        .get_element_by_id("loading")
-        .expect("should have #loading on the page")
-        .dyn_ref::<HtmlElement>()
-        .expect("#loading should be an `HtmlElement`")
-        .style()
-        .set_property("display", "none")?;
-
-    let body: HtmlElement = document.body().expect("document should have a body");
-
-    // Manufacture the element we're gonna append
-    let p_tag: Element = document.create_element("p")?;
-    p_tag.set_inner_html("Hello from Rust!");
-
-    body.append_child(&p_tag)?;
-    Ok(())
+/// Converts a midi note to frequency
+///
+/// A midi note is an integer, generally in the range of 21 to 108
+pub fn midi_to_freq(note: u8) -> f32 {
+    27.5 * 2f32.powf((note as f32 - 21.0) / 12.0)
 }
 
-// Set up a clock on our page and update it each second to ensure it's got
-// an accurate date.
-//
-// Note the usage of `Closure` here because the closure is "long lived",
-// basically meaning it has to persist beyond the call to this one function.
-// Also of note here is the `.as_ref().unchecked_ref()` chain, which is how
-// you can extract `&Function`, what `web-sys` expects, from a `Closure`
-// which only hands you `&JsValue` via `AsRef`.
-fn setup_clock(window: &Window, document: &Document) -> Result<(), JsValue> {
-    let current_time = document
-        .get_element_by_id("current-time")
-        .expect("should have #current-time on the page");
-    update_time(&current_time);
-    let a = Closure::wrap(Box::new(move || update_time(&current_time)) as Box<dyn Fn()>);
-    window
-        .set_interval_with_callback_and_timeout_and_arguments_0(a.as_ref().unchecked_ref(), 1000)?;
-    fn update_time(current_time: &Element) {
-        current_time.set_inner_html(&String::from(
-            Date::new_0().to_locale_string("en-GB", &JsValue::undefined()),
-        ));
+#[wasm_bindgen]
+pub struct FmOsc {
+    ctx: AudioContext,
+    /// The primary oscillator.  This will be the fundamental frequency
+    primary: web_sys::OscillatorNode,
+
+    /// Overall gain (volume) control
+    gain: web_sys::GainNode,
+
+    /// Amount of frequency modulation
+    fm_gain: web_sys::GainNode,
+
+    /// The oscillator that will modulate the primary oscillator's frequency
+    fm_osc: web_sys::OscillatorNode,
+
+    /// The ratio between the primary frequency and the fm_osc frequency.
+    ///
+    /// Generally fractional values like 1/2 or 1/4 sound best
+    fm_freq_ratio: f32,
+
+    fm_gain_ratio: f32,
+}
+
+impl Drop for FmOsc {
+    fn drop(&mut self) {
+        let _ = self.ctx.close();
+    }
+}
+
+#[wasm_bindgen]
+impl FmOsc {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Result<FmOsc, JsValue> {
+        let ctx = web_sys::AudioContext::new()?;
+
+        // Create our web audio objects.
+        let primary = ctx.create_oscillator()?;
+        let fm_osc = ctx.create_oscillator()?;
+        let gain = ctx.create_gain()?;
+        let fm_gain = ctx.create_gain()?;
+
+        // Some initial settings:
+        primary.set_type(OscillatorType::Sine);
+        primary.frequency().set_value(440.0); // A4 note
+        gain.gain().set_value(0.0); // starts muted
+        fm_gain.gain().set_value(0.0); // no initial frequency modulation
+        fm_osc.set_type(OscillatorType::Sine);
+        fm_osc.frequency().set_value(0.0);
+
+        // Connect the nodes up!
+
+        // The primary oscillator is routed through the gain node, so that
+        // it can control the overall output volume.
+        primary.connect_with_audio_node(&gain)?;
+
+        // Then connect the gain node to the AudioContext destination (aka
+        // your speakers).
+        gain.connect_with_audio_node(&ctx.destination())?;
+
+        // The FM oscillator is connected to its own gain node, so it can
+        // control the amount of modulation.
+        fm_osc.connect_with_audio_node(&fm_gain)?;
+
+        // Connect the FM oscillator to the frequency parameter of the main
+        // oscillator, so that the FM node can modulate its frequency.
+        fm_gain.connect_with_audio_param(&primary.frequency())?;
+
+        // Start the oscillators!
+
+        // This starts the speaker volume
+        primary.start()?;
+
+        // This ___?
+        fm_osc.start()?;
+
+        Ok(FmOsc {
+            ctx,
+            primary,
+            gain,
+            fm_gain,
+            fm_osc,
+            fm_freq_ratio: 0.0,
+            fm_gain_ratio: 0.0,
+        })
     }
 
-    // The instance of `Closure` that we created will invalidate its
-    // corresponding JS callback whenever it is dropped, so if we were to
-    // normally return from `setup_clock` then our registered closure will
-    // raise an exception when invoked.
-    //
-    // Normally we'd store the handle to later get dropped at an appropriate
-    // time but for now we want it to be a global handler so we use the
-    // `forget` method to drop it without invalidating the closure. Note that
-    // this is leaking memory in Rust, so this should be done judiciously!
-    a.forget();
+    /// Sets the gain for this oscillator, between 0.0 and 1.0.
+    #[wasm_bindgen]
+    pub fn set_gain(&self, mut gain: f32) {
+        // clamp
+        if gain > 1.0 {
+            gain = 1.0;
+        }
+        if gain < 0.0 {
+            gain = 0.0;
+        }
+        self.gain.gain().set_value(gain);
+    }
 
-    Ok(())
-}
+    #[wasm_bindgen]
+    pub fn set_primary_frequency(&self, mut freq: f32) {
+        // clamp the PITCH to Double high C
+        // 76th key on
+        if freq > 2093.005 {
+            freq = 2093.005;
+        }
+        if freq < 0.0 {
+            freq = 0.0;
+        }
+        self.primary.frequency().set_value(freq);
 
-// We also want to count the number of times that our green square has been
-// clicked. Our callback will update the `#num-clicks` div.
-//
-// This is pretty similar above, but showing how closures can also implement
-// `FnMut()`.
-fn setup_clicker(document: &Document) {
-    let num_clicks = document
-        .get_element_by_id("num-clicks")
-        .expect("should have #num-clicks on the page");
+        // The frequency of the FM oscillator depends on the frequency of the
+        // primary oscillator, so we update the frequency of both in this method.
+        self.fm_osc.frequency().set_value(self.fm_freq_ratio * freq);
+        self.fm_gain.gain().set_value(self.fm_gain_ratio * freq);
+    }
 
-    let mut clicks = 0;
+    #[wasm_bindgen]
+    pub fn set_note(&self, note: u8) {
+        let freq = midi_to_freq(note);
+        self.set_primary_frequency(freq);
+    }
 
-    let a = Closure::wrap(Box::new(move || {
-        clicks += 1;
-        console::log_1(&format!("setup_clicker::clicks::{}", clicks).into());
-        num_clicks.set_inner_html(&clicks.to_string());
-    }) as Box<dyn FnMut()>);
+    /// This should be between 0 and 1, though higher values are accepted.
+    #[wasm_bindgen]
+    pub fn set_fm_amount(&mut self, amt: f32) {
+        self.fm_gain_ratio = amt;
 
-    // DERP
-    // Attach onclick handler to #green-square
-    document
-        .get_element_by_id("green-square")
-        .expect("should have #green-square on the page")
-        .dyn_ref::<HtmlElement>()
-        .expect("#green-square be an `HtmlElement`")
-        .set_onclick(Some(a.as_ref().unchecked_ref()));
+        self.fm_gain
+            .gain()
+            .set_value(self.fm_gain_ratio * self.primary.frequency().value());
+    }
 
-    // See comments in `setup_clock` above for why we use `a.forget()`.
-    a.forget();
+    /// This should be between 0 and 1, though higher values are accepted.
+    #[wasm_bindgen]
+    pub fn set_fm_frequency(&mut self, amt: f32) {
+        self.fm_freq_ratio = amt;
+        self.fm_osc
+            .frequency()
+            .set_value(self.fm_freq_ratio * self.primary.frequency().value());
+    }
 }
