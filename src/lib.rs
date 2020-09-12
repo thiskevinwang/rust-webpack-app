be4c43efa58a1cab5554cb1f15f75cf127c1c842
Kevin Wang <kwangsan@gmail.com>
2020-09-12 10:58:20 -0400

    paint POC

--- src/lib.rs
+++ src/lib.rs
@@ -1,152 +1,66 @@
+use std::cell::Cell;
+use std::rc::Rc;
 use wasm_bindgen::prelude::*;
-use web_sys::{AudioContext, OscillatorType};
+use wasm_bindgen::JsCast;
 
-/// Converts a midi note to frequency
-///
-/// A midi note is an integer, generally in the range of 21 to 108
-pub fn midi_to_freq(note: u8) -> f32 {
-    27.5 * 2f32.powf((note as f32 - 21.0) / 12.0)
-}
+// Websocket endpoint: "ws://localhost:3000/chat"
+// Websocket send body: tuple
+//   [number, number]
+// Websocket message: json
+//   { "data": "<User#92>: 730,53" }
 
-#[wasm_bindgen]
-pub struct FmOsc {
-    ctx: AudioContext,
-    /// The primary oscillator.  This will be the fundamental frequency
-    primary: web_sys::OscillatorNode,
-
-    /// Overall gain (volume) control
-    gain: web_sys::GainNode,
-
-    /// Amount of frequency modulation
-    fm_gain: web_sys::GainNode,
-
-    /// The oscillator that will modulate the primary oscillator's frequency
-    fm_osc: web_sys::OscillatorNode,
-
-    /// The ratio between the primary frequency and the fm_osc frequency.
-    ///
-    /// Generally fractional values like 1/2 or 1/4 sound best
-    fm_freq_ratio: f32,
-
-    fm_gain_ratio: f32,
-}
-
-impl Drop for FmOsc {
-    fn drop(&mut self) {
-        let _ = self.ctx.close();
+#[wasm_bindgen(start)]
+pub fn start() -> Result<(), JsValue> {
+    let document = web_sys::window().unwrap().document().unwrap();
+    let canvas = document
+        .create_element("canvas")?
+        .dyn_into::<web_sys::HtmlCanvasElement>()?;
+    document.body().unwrap().append_child(&canvas)?;
+    canvas.set_width(640);
+    canvas.set_height(480);
+    canvas.style().set_property("border", "solid")?;
+    let context = canvas
+        .get_context("2d")?
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()?;
+    let context = Rc::new(context);
+    let pressed = Rc::new(Cell::new(false));
+    {
+        let context = context.clone();
+        let pressed = pressed.clone();
+        let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
+            context.begin_path();
+            context.move_to(event.offset_x() as f64, event.offset_y() as f64);
+            pressed.set(true);
+        }) as Box<dyn FnMut(_)>);
+        canvas.add_event_listener_with_callback("mousedown", closure.as_ref().unchecked_ref())?;
+        closure.forget();
     }
-}
-
-#[wasm_bindgen]
-impl FmOsc {
-    #[wasm_bindgen(constructor)]
-    pub fn new() -> Result<FmOsc, JsValue> {
-        let ctx = web_sys::AudioContext::new()?;
-
-        // Create our web audio objects.
-        let primary = ctx.create_oscillator()?;
-        let fm_osc = ctx.create_oscillator()?;
-        let gain = ctx.create_gain()?;
-        let fm_gain = ctx.create_gain()?;
-
-        // Some initial settings:
-        primary.set_type(OscillatorType::Sine);
-        primary.frequency().set_value(440.0); // A4 note
-        gain.gain().set_value(0.0); // starts muted
-        fm_gain.gain().set_value(0.0); // no initial frequency modulation
-        fm_osc.set_type(OscillatorType::Sine);
-        fm_osc.frequency().set_value(0.0);
-
-        // Connect the nodes up!
-
-        // The primary oscillator is routed through the gain node, so that
-        // it can control the overall output volume.
-        primary.connect_with_audio_node(&gain)?;
-
-        // Then connect the gain node to the AudioContext destination (aka
-        // your speakers).
-        gain.connect_with_audio_node(&ctx.destination())?;
-
-        // The FM oscillator is connected to its own gain node, so it can
-        // control the amount of modulation.
-        fm_osc.connect_with_audio_node(&fm_gain)?;
-
-        // Connect the FM oscillator to the frequency parameter of the main
-        // oscillator, so that the FM node can modulate its frequency.
-        fm_gain.connect_with_audio_param(&primary.frequency())?;
-
-        // Start the oscillators!
-
-        // This starts the speaker volume
-        primary.start()?;
-
-        // This ___?
-        fm_osc.start()?;
-
-        Ok(FmOsc {
-            ctx,
-            primary,
-            gain,
-            fm_gain,
-            fm_osc,
-            fm_freq_ratio: 0.0,
-            fm_gain_ratio: 0.0,
-        })
+    {
+        let context = context.clone();
+        let pressed = pressed.clone();
+        let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
+            if pressed.get() {
+                context.line_to(event.offset_x() as f64, event.offset_y() as f64);
+                context.stroke();
+                context.begin_path();
+                context.move_to(event.offset_x() as f64, event.offset_y() as f64);
+            }
+        }) as Box<dyn FnMut(_)>);
+        canvas.add_event_listener_with_callback("mousemove", closure.as_ref().unchecked_ref())?;
+        closure.forget();
+    }
+    {
+        let context = context.clone();
+        let pressed = pressed.clone();
+        let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
+            pressed.set(false);
+            context.line_to(event.offset_x() as f64, event.offset_y() as f64);
+            context.stroke();
+        }) as Box<dyn FnMut(_)>);
+        canvas.add_event_listener_with_callback("mouseup", closure.as_ref().unchecked_ref())?;
+        closure.forget();
     }
 
-    /// Sets the gain for this oscillator, between 0.0 and 1.0.
-    #[wasm_bindgen]
-    pub fn set_gain(&self, mut gain: f32) {
-        // clamp
-        if gain > 1.0 {
-            gain = 1.0;
-        }
-        if gain < 0.0 {
-            gain = 0.0;
-        }
-        self.gain.gain().set_value(gain);
-    }
-
-    #[wasm_bindgen]
-    pub fn set_primary_frequency(&self, mut freq: f32) {
-        // clamp the PITCH to Double high C
-        // 76th key on
-        if freq > 2093.005 {
-            freq = 2093.005;
-        }
-        if freq < 0.0 {
-            freq = 0.0;
-        }
-        self.primary.frequency().set_value(freq);
-
-        // The frequency of the FM oscillator depends on the frequency of the
-        // primary oscillator, so we update the frequency of both in this method.
-        self.fm_osc.frequency().set_value(self.fm_freq_ratio * freq);
-        self.fm_gain.gain().set_value(self.fm_gain_ratio * freq);
-    }
-
-    #[wasm_bindgen]
-    pub fn set_note(&self, note: u8) {
-        let freq = midi_to_freq(note);
-        self.set_primary_frequency(freq);
-    }
-
-    /// This should be between 0 and 1, though higher values are accepted.
-    #[wasm_bindgen]
-    pub fn set_fm_amount(&mut self, amt: f32) {
-        self.fm_gain_ratio = amt;
-
-        self.fm_gain
-            .gain()
-            .set_value(self.fm_gain_ratio * self.primary.frequency().value());
-    }
-
-    /// This should be between 0 and 1, though higher values are accepted.
-    #[wasm_bindgen]
-    pub fn set_fm_frequency(&mut self, amt: f32) {
-        self.fm_freq_ratio = amt;
-        self.fm_osc
-            .frequency()
-            .set_value(self.fm_freq_ratio * self.primary.frequency().value());
-    }
+    Ok(())
 }
